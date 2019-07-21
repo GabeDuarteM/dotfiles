e9ad6240ccfa8e7ee742e42b683236a0d4f0fdfb
Gabriel Duarte <gabrielqduarte@gmail.com>
2019-07-21 20:12:32 +0200

    tmux: add tmuxinator layouts

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -5,6 +5,8 @@ ln -f $DOTFILES_FOLDER/files/.profile ~
 ln -f $DOTFILES_FOLDER/files/.tmux.conf ~
 ln -f $DOTFILES_FOLDER/files/.zpreztorc ~
 ln -f $DOTFILES_FOLDER/files/.zshrc ~
+ln -f $DOTFILES_FOLDER/files/.tmuxinator/code.yml ~/.tmuxinator/code.yml
+ln -f $DOTFILES_FOLDER/files/.tmuxinator/term.yml ~/.tmuxinator/term.yml
 ln -f $DOTFILES_FOLDER/files/alacritty.yml ~/.config/alacritty
 ln -f $DOTFILES_FOLDER/files/vim/.vimrc ~/.config/nvim/init.vim
 ln -f $DOTFILES_FOLDER/files/vim/coc-settings.json ~/.config/nvim
