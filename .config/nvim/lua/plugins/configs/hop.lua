5439780e9da9ad638f26f4c3051df09418415388
Gabriel Duarte <gabrielqduarte@gmail.com>
2021-11-22 15:12:34 +0100

    remove leader from hop map

--- .config/nvim/lua/plugins/configs/hop.lua
+++ .config/nvim/lua/plugins/configs/hop.lua
@@ -1,6 +1,6 @@
 local utils = require('utils')
 
-utils.nnoremap('<leader>s',':HopChar1<cr>')
+utils.nnoremap('s',':HopChar1<cr>')
 utils.nnoremap('<leader>j',':HopLine<cr>')
 utils.nnoremap('<leader>k',':HopLine<cr>')
 utils.onoremap('z',':HopChar1<cr>')
