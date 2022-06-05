80b820cea5299137eb2096ebb220621f51fe19ca
Gabriel Duarte <gabrielqduarte@gmail.com>
2022-06-06 00:41:19 +0200

    fix global configs for nvim tree

--- .config/nvim/lua/plugins/configs/nvim-tree.lua
+++ .config/nvim/lua/plugins/configs/nvim-tree.lua
@@ -4,34 +4,10 @@ utils.nnoremap("<C-n>", "NvimTreeToggle")
 utils.nnoremap("<leader>r",':NvimTreeRefresh<CR>')
 utils.nnoremap("<leader>n",':NvimTreeFindFileToggle<CR>')
 
-vim.g.nvim_tree_respect_buf_cwd = 1
-vim.g.nvim_tree_icons = {
-  default = '',
-  symlink = '',
-  git = {
-    unstaged = "✗",
-    staged = "✓",
-    unmerged = "",
-    renamed = "➜",
-    untracked = "★",
-    deleted = "",
-    ignored = "◌"
-  },
-  folder = {
-    arrow_open = "",
-    arrow_closed = "",
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-    symlink_open = "",
-  }
-}
-
 require'nvim-tree'.setup { 
     update_cwd = true,
     disable_netrw = false,
+    respect_buf_cwd = true,
     view = {
       width = 70,
     },
@@ -40,5 +16,32 @@ require'nvim-tree'.setup {
         quit_on_open = true,
         resize_window = true
       }
+    },
+    renderer = {
+      icons = {
+        glyphs = {
+          default = '',
+          symlink = '',
+          git = {
+            unstaged = "✗",
+            staged = "✓",
+            unmerged = "",
+            renamed = "➜",
+            untracked = "★",
+            deleted = "",
+            ignored = "◌"
+          },
+          folder = {
+            arrow_open = "",
+            arrow_closed = "",
+            default = "",
+            open = "",
+            empty = "",
+            empty_open = "",
+            symlink = "",
+            symlink_open = "",
+        }
+      }
     }
+  }
 }
