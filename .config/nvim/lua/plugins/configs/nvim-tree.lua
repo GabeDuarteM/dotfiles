54963212310026e909ff244a56d531acdd6d23c5
Gabriel Duarte <gabrielqduarte@gmail.com>
2022-06-06 22:26:36 +0200

    add search inside node to nvimtree

--- .config/nvim/lua/plugins/configs/nvim-tree.lua
+++ .config/nvim/lua/plugins/configs/nvim-tree.lua
@@ -4,12 +4,24 @@ utils.nnoremap("<C-n>", "NvimTreeToggle")
 utils.nnoremap("<leader>r",':NvimTreeRefresh<CR>')
 utils.nnoremap("<leader>n",':NvimTreeFindFileToggle<CR>')
 
+local function search_inside_node(node)
+  -- amount of <left> needed to put the cursor before the path
+  local leftAmount = string.rep('<left>', string.len(node.absolute_path) + 1)
+
+  vim.api.nvim_input(':CtrlSF  ' .. node.absolute_path .. leftAmount)
+end
+
 require'nvim-tree'.setup { 
     update_cwd = true,
     disable_netrw = false,
     respect_buf_cwd = true,
     view = {
       width = 70,
+      mappings = {
+        list = {
+          { key = "<space>f", action = "search_inside_node", action_cb = search_inside_node },
+        }
+      }
     },
     actions = {
       open_file = {
