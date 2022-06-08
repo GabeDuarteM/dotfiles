c3ca986f29e352bb3e0ee469bc4a6dd9df3e6ed3
Gabriel Duarte <gabrielqduarte@gmail.com>
2022-06-08 18:32:30 +0000

    add line mappings for lightspeed

--- .config/nvim/lua/plugins/configs/lightspeed.lua
+++ .config/nvim/lua/plugins/configs/lightspeed.lua
@@ -1,6 +1,9 @@
-local utils = require('utils')
-
-utils.nnoremap("s", "<Plug>Lightspeed_omni_s", { silent = true })
+vim.api.nvim_set_keymap("n", "s", "<Plug>Lightspeed_omni_s", { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>j", "<Plug>Lightspeed_omni_s<CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>k", "<Plug>Lightspeed_omni_s<CR>", { silent = true })
+vim.api.nvim_set_keymap("o", "<leader>j", "<Plug>Lightspeed_omni_s<CR>", { silent = true })
+vim.api.nvim_set_keymap("o", "<leader>k", "<Plug>Lightspeed_omni_s<CR>", { silent = true })
+vim.api.nvim_set_keymap("o", "z", "<Plug>Lightspeed_omni_s<CR>", { silent = true })
 
 require 'lightspeed'.setup {
   ignore_case = true,
