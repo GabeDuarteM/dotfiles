de2bb6ca2797d69ac8661565bc6024ce9eade8ab
Gabriel Duarte <gabrielqduarte@gmail.com>
2023-01-06 11:57:11 +0000

    add args to the run script:wq

--- .config/.functions/run.sh
+++ .config/.functions/run.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-run() {
-  if [ -f "Cargo.toml" ]; then
-    cargo run
+run () {
+  # Store the arguments in a variable
+  args=$@
+
+  if [ -f "Cargo.toml" ]
+  then
+    cargo run $args
     return
-  elif [ -f "pnpm-lock.yaml" ]; then
-    pnpm start
+  elif [ -f "pnpm-lock.yaml" ]
+  then
+    pnpm start $args
     return
-  elif [ -f "yarn.lock" ]; then
-    yarn start
+  elif [ -f "yarn.lock" ]
+  then
+    yarn start $args
     return
-  elif [ -f "package.json" ]; then
-    npm run start
+  elif [ -f "package.json" ]
+  then
+    npm run start $args
     return
-  elif [ -f "go.mod" ]; then
-    go run .
+  elif [ -f "go.mod" ]
+  then
+    go run . $args
     return
   else
     echo "No build script found"
