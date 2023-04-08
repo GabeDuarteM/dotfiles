0d54c2d88693f93c6acfce0812dba4dcfbcc8a5a
Gabriel Duarte <gabrielqduarte@gmail.com>
2023-04-08 11:40:51 +0000

    add workfolder and project env var verification before running it

--- .config/.functions/cd-into-projects.sh
+++ .config/.functions/cd-into-projects.sh
@@ -1,26 +1,37 @@
 _getProjectBase() {
-  echo $1/$(find $1 -maxdepth 1 -type d ! -path $1 -exec basename {} \; | fzf --no-preview --height=40% --header="Select the project:" --reverse --query "$2" --select-1)
+	if [ -z "$1" ]; then
+		echo "No projects folder specified"
+		return
+	fi
+
+	echo $1/$(find $1 -maxdepth 1 -type d ! -path $1 -exec basename {} \; | fzf --no-preview --height=40% --header="Select the project:" --reverse --query "$2" --select-1)
 }
 
 _getProject() {
-  _getProjectBase "$PROJECTS_FOLDER" "$1"
+	_getProjectBase "$PROJECTS_FOLDER" "$1"
 }
 
 _getWorkProject() {
-  _getProjectBase "$WORK_FOLDER" "$1"
+	_getProjectBase "$WORK_FOLDER" "$1"
 }
 
 function p() {
-  local dir=$(_getProject $1)
-  if [ "$dir" != "$PROJECTS_FOLDER/" ]; then;
-    cd $dir
-  fi
+	local dir=$(_getProject $1)
+
+	if [ "$dir" != "$PROJECTS_FOLDER/" ]; then
+		cd $dir
+	fi
 }
 
-# cd into some folder inside the work projects folder
 function wp() {
-  local dir=$(_getWorkProject $1)
-  if [ "$dir" != "$WORK_FOLDER/" ]; then;
-    cd $dir
-  fi
+	if [ -z "$WORK_FOLDER" ]; then
+		echo "WORK_FOLDER env variable is not set"
+		return
+	fi
+
+	local dir=$(_getWorkProject $1)
+
+	if [ "$dir" != "$WORK_FOLDER/" ]; then
+		cd $dir
+	fi
 }
