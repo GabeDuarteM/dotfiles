9d96a53ded6f78d8f27a10a3ac89944c2b782a5c
Gabriel Duarte <gabrielqduarte@gmail.com>
2023-04-26 17:03:48 +0000

    use git with https on first run

--- .config/setup-dotfiles.sh
+++ .config/setup-dotfiles.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-git clone --bare git@github.com:GabrielDuarteM/dotfiles.git $HOME/.config/.dotfiles
 shopt -s expand_aliases
 
 alias config='/usr/bin/git --git-dir=$HOME/.config/.dotfiles/ --work-tree=$HOME'
+
+# We setup with the https url so that we don't need to have ssh keys setup beforehand.
+# Later we change the remote url to ssh, so that we can push/pull changes without auth.
+git clone --bare https://github.com/GabeDuarteM/dotfiles.git $HOME/.config/.dotfiles
 
 cd $HOME
 
@@ -16,5 +19,7 @@ if test -f "$HOME/.zshrc"; then
 fi
 
 config checkout
+
+config remote set-url origin git@github.com:GabeDuarteM/dotfiles.git
 
 bash $HOME/.config/setup-install-packages.sh
