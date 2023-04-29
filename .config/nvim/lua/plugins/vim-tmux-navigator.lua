565dd714ef2e437fd9213753aa02be887d50eb18
Gabriel Duarte <gabrielqduarte@gmail.com>
2023-04-29 14:43:36 +0000

    add tmux navigator

---
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,10 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  lazy = false,
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+  },
+}
