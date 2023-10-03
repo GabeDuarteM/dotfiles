70ea04148e1a9b281f83c4c7c1a641526997e0b3
Gabriel Duarte <gabrielqduarte@gmail.com>
2023-10-04 01:40:32 +0200

    fix cmp to not select the first one automatically and not complete on <CR>

---
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,20 @@
+return {
+  "hrsh7th/nvim-cmp",
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.mapping["<CR>"] = cmp.mapping({
+      i = function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        else
+          fallback()
+        end
+      end,
+      s = cmp.mapping.confirm({ select = true }),
+      c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    })
+
+    opts.completion.completeopt = opts.completion.completeopt .. ",noselect"
+  end,
+}
