175687287ef2d0c274b4cad50bab1ab9e476b348
Gabriel Duarte <gabrielqduarte@gmail.com>
2023-10-04 01:41:20 +0200

    switch to persisted
    has git branch configs and seems more maintained

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("gabe_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("disable_session_persisted"),
+  pattern = { "gitcommit" },
+  callback = function()
+    require("persisted").stop()
+  end,
+})
---
+++ .config/nvim/lua/plugins/persisted.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "folke/persistence.nvim",
+    enabled = false,
+  },
+  {
+    "olimorris/persisted.nvim",
+    event = "BufReadPre",
+    opts = {
+      options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" },
+      use_git_branch = true,
+      should_autosave = function()
+        -- do not autosave if the alpha dashboard is the current filetype
+        if vim.bo.filetype == "alpha" then
+          return false
+        end
+        return true
+      end,
+      on_autoload_no_session = function()
+        vim.notify("No existing session to load.")
+      end,
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>qs", function() require("persisted").load() end, desc = "Restore Session" },
+      { "<leader>ql", function() require("persisted").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persisted").stop() end, desc = "Don't Save Current Session" },
+    },
+  },
+}
