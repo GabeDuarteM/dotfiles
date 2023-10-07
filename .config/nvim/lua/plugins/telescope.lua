7a36d6e8f7851bfce25cbb5093d57820f44fc55e
Gabriel Duarte <gabrielqduarte@gmail.com>
2023-10-07 18:52:15 +0200

    remove vim-rzip

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,23 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.1",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  keys = { { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find git files" } },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.1",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = { { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find git files" } },
+    opts = {
+      extensions = {
+        frecency = {
+          default_workspace = "CWD",
+        },
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope-frecency.nvim",
+    config = function()
+      require("telescope").load_extension("frecency")
+    end,
+    dependencies = { "kkharji/sqlite.lua" },
+    keys = { { "<leader><leader>", "<cmd>Telescope frecency<cr>", desc = "Find files by frecency" } },
+  },
 }
--- .config/nvim/lua/plugins/vim-rzip.lua
+++
@@ -1,6 +0,0 @@
--- Vim plugin for working with RAR and ZIP archives
--- Useful for yarn pnp
-return {
-  "lbrayner/vim-rzip",
-  enabled = false,
-}
