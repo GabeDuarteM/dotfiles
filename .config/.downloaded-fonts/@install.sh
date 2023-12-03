b72117ebea14f88dcc3eb9e7e834c00c6dcad03c
Gabriel Duarte <gabrielqduarte@gmail.com>
2023-12-03 01:43:05 +0100

    setup: downgrade phosphor font and improve font install script

--- .config/.downloaded-fonts/@install.sh
+++ .config/.downloaded-fonts/@install.sh
@@ -2,7 +2,7 @@
 
 # Copy all files except "@install.sh"
 shopt -s extglob
-cp !("@install.sh") ~/.local/share/fonts
+cp ~/.config/.downloaded-fonts/!("@install.sh") ~/.local/share/fonts
 
 # Run fc-cache
 fc-cache -frv
