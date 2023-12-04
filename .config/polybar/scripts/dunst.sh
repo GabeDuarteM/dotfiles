00e9d31c75cd714f1e6038ddd80f7e6d95340068
Gabriel Duarte <gabrielqduarte@gmail.com>
2023-12-04 02:12:21 +0100

    polybar: improve the notification module
    allow right clicking to toggle dnd, and also show the amount of notifications

---
+++ .config/polybar/scripts/dunst.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/.zshenv
+
+count=$(~/.config/eww/.venv/bin/python ~/.config/eww/modules/disclose/scripts/logger.py stats | jq .total)
+is_dnd=$(dunstctl is-paused)
+
+icon=""
+
+if [ "$is_dnd" = "true" ]; then
+	icon="%{F#FF0000}%{F-}"
+fi
+
+if [ "$count" -gt 0 ]; then
+	echo "$icon  $count"
+else
+	echo $icon
+fi
