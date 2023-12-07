a0f0cad584796a53f9ec1d031c6da552c6590426
Gabriel Duarte <gabrielqduarte@gmail.com>
2023-12-07 21:27:04 +0100

    eww: rename index files and define monitors

--- .config/eww/eww.sh
+++ .config/eww/eww.sh
@@ -1,2 +1,4 @@
 source ~/.zshenv
-~/.config/eww/.venv/bin/python3 ~/.config/eww/modules/disclose/scripts/logger.py init
+
+~/.config/eww/.venv/bin/python3 ~/.config/eww/modules/disclose/scripts/logger.py init &
+eww daemon &
