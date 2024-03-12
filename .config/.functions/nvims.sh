92dd87fd7239cd20081d34dd3660feee4ab2e6f5
Gabriel Duarte <gabrielqduarte@gmail.com>
2024-03-12 12:02:28 +0100

    nvims: fixed formatting and kickstart repo name

--- .config/.functions/nvims.sh
+++ .config/.functions/nvims.sh
@@ -1,11 +1,11 @@
 function nvims() {
-  items=("default" "kickstart" "LazyVim")
-  config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
-  if [[ -z $config ]]; then
-    echo "Nothing selected"
-    return 0
-  elif [[ $config == "default" ]]; then
-    config=""
-  fi
-  NVIM_APPNAME=$config nvim $@
+	items=("default" "kickstart.nvim" "LazyVim")
+	config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+	if [[ -z $config ]]; then
+		echo "Nothing selected"
+		return 0
+	elif [[ $config == "default" ]]; then
+		config=""
+	fi
+	NVIM_APPNAME=$config nvim $@
 }
