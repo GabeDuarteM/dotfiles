7bdc3025e449acdd1e2674173201bdddd966b483
Gabriel Duarte <gabrielqduarte@gmail.com>
2024-07-05 12:47:54 +0200

    nvim: disable inlay hints

--- .config/nvim/lua/plugins/lsp_servers.lua
+++ .config/nvim/lua/plugins/lsp_servers.lua
@@ -3,6 +3,10 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
+      inlay_hints = {
+        enabled = false,
+        exclude = {}, -- filetypes for which you don't want to enable inlay hints
+      },
       ---@type lspconfig.options
       servers = {
         rust_analyzer = {},
