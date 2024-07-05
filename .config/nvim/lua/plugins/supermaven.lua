16b44ca3c76618f81e8547ac594ccb6fcd105d0a
Gabriel Duarte <gabrielqduarte@gmail.com>
2024-07-05 12:49:40 +0200

    nvim: add supermaven

---
+++ .config/nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,46 @@
+-- https://github.com/LazyVim/LazyVim/pull/3491
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({})
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "supermaven",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("supermaven"))
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    optional = true,
+    opts = function(_, opts)
+      vim.list_extend(opts.routes, {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "Starting Supermaven" },
+              { find = "Supermaven Free Tier" },
+            },
+          },
+          skip = true,
+        },
+      })
+    end,
+  },
+}
