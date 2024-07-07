8d5d40b3abb2695b9258a6e835e870ac11819973
Gabriel Duarte <gabrielqduarte@gmail.com>
2024-07-08 01:10:25 +0200

    setup: add wayland alternative to ocr

--- .local/bin/ocr-with-tesseract.sh
+++ .local/bin/ocr-with-tesseract.sh
@@ -2,8 +2,12 @@
 
 local imagefile="/tmp/sloppy.$RANDOM.png"
 local text="/tmp/translation"
-# local slop=$(slop -f "%g") || exit 1
-# maim -g "$slop" $imagefile
-flameshot gui -p "$imagefile" 2>/dev/null || exit 1
+
+if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+  grimblast save area $imagefile 
+else
+  flameshot gui -p "$imagefile" 2>/dev/null || exit 1
+fi
+
 tesseract $imagefile $text 2>/dev/null
 cat $text".txt" | xclip -selection c
