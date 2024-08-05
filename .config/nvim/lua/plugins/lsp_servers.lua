45e0087935e7b856c58accf01ff52ae52814f315
Gabriel Duarte <gabrielqduarte@gmail.com>
2024-08-05 22:13:48 +0200

    nvim: remove unecessary extra {} on lsp_servers

--- .config/nvim/lua/plugins/lsp_servers.lua
+++ .config/nvim/lua/plugins/lsp_servers.lua
@@ -1,19 +1,16 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      inlay_hints = {
-        enabled = false,
-        exclude = {}, -- filetypes for which you don't want to enable inlay hints
-      },
-      ---@type lspconfig.options
-      servers = {
-        rust_analyzer = {},
-        tailwindcss = {},
-        astro = {},
-        gopls = {},
-      },
+  "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
+  opts = {
+    inlay_hints = {
+      enabled = false,
+      exclude = {}, -- filetypes for which you don't want to enable inlay hints
+    },
+    ---@type lspconfig.options
+    servers = {
+      tailwindcss = {},
+      astro = {},
+      gopls = {},
     },
   },
 }
