aeeb47f5986f6a484ed553f094b86447918d4617
Gabriel Duarte <gabrielqduarte@gmail.com>
2024-08-05 22:16:45 +0200

    work: move to foot and use hyprctl to script better on start-work-apps

--- work/start-work-apps.sh
+++ work/start-work-apps.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# Brave
+hyprctl dispatch workspace 2
+hyprctl dispatch exec -- "/usr/bin/brave '--profile-directory=Profile 1'"
+# the workspace 2 directive doesn't work as brave probably forks their PID,
+# so we just go to ws 2 and wait until it can launch
+sleep 0.2
+
 # Slack
 /usr/bin/slack &
 
-# Brave
-/usr/bin/brave '--profile-directory=Profile 1' &
-
 # MS Teams
+# function is_msteams_running() {
+#   hyprctl clients -j | jq ".[] | select(.initialTitle | contains(\"Microsoft Teams\"))"
+# }
+# if [[ ! is_msteams_running ]]; then
+# echo "MS Teams not running, launching"
 /usr/bin/brave "--profile-directory=Profile 1" --app-id=cifhbcnohmdccbgoicgdjpfamggdegmo &
 
-# Alacritty
-/usr/bin/alacritty --title Code -e /usr/bin/tmux new-session -A -D -s code -c /home/gabe/work/urbyo/platform &
+# Terminal
+/usr/bin/foot --title "foot: Code" -e /usr/bin/tmux new-session -A -D -s code -c /home/gabe/work/urbyo/platform &
