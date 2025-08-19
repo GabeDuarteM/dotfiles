b76e1841980ecfd04468845ad9e2410b6a486a37
Gabriel Duarte <gabrielqduarte@gmail.com>
2025-08-19 14:36:17 +0200

    formatting

--- .config/.functions/cd-into-projects.sh
+++ .config/.functions/cd-into-projects.sh
@@ -1,37 +1,21 @@
 _getProjectBase() {
-	if [ -z "$1" ]; then
-		echo "No projects folder specified"
-		return
-	fi
+  if [ -z "$1" ]; then
+    echo "No projects folder specified"
+    return
+  fi
 
-	echo $1/$(find $1 -maxdepth 1 -type d ! -path $1 -exec basename {} \; | fzf --no-preview --height=40% --header="Select the project:" --reverse --query "$2" --select-1)
+  echo "$1"/"$(find "$1" -maxdepth 1 -type d ! -path "$1" -exec basename {} \; | fzf --no-preview --height=40% --header="Select the project:" --reverse --query "$2" --select-1)"
 }
 
 _getProject() {
-	_getProjectBase "$PROJECTS_FOLDER" "$1"
-}
-
-_getWorkProject() {
-	_getProjectBase "$WORK_FOLDER" "$1"
+  _getProjectBase "$PROJECTS_FOLDER" "$1"
 }
 
 function p() {
-	local dir=$(_getProject $1)
+  local dir
+  dir=$(_getProject "$1")
 
-	if [ "$dir" != "$PROJECTS_FOLDER/" ]; then
-		cd $dir
-	fi
-}
-
-function wp() {
-	if [ -z "$WORK_FOLDER" ]; then
-		echo "WORK_FOLDER env variable is not set"
-		return
-	fi
-
-	local dir=$(_getWorkProject $1)
-
-	if [ "$dir" != "$WORK_FOLDER/" ]; then
-		cd $dir
-	fi
+  if [ "$dir" != "$PROJECTS_FOLDER/" ]; then
+    cd "$dir" || return
+  fi
 }
