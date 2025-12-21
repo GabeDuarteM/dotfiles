68ebcb85e99c0cfcae53a0b0a0118b6b8df89dbe
Gabriel Duarte <gabrielqduarte@gmail.com>
2025-12-21 18:06:44 +0100

    cleanup unused configs

--- .config/eww/eww.sh
+++
@@ -1,4 +0,0 @@
-source ~/.zshenv
-
-~/.config/eww/.venv/bin/python3 ~/.config/eww/modules/disclose/scripts/logger.py init &
-eww daemon &
--- .config/polybar/scripts/eww_wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-eww open $1-closer --toggle --screen $MONITOR
-eww open $1 --toggle --screen $MONITOR
