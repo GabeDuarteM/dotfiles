3771deaf152182a57aa68a64d9a38d774b106d09
Gabriel Duarte <gabrielqduarte@gmail.com>
2026-03-02 14:06:57 +0100

    nvim: fix UI stutter on tsx files

--- .config/nvim/lua/plugins/lsp_servers.lua
+++ .config/nvim/lua/plugins/lsp_servers.lua
@@ -8,6 +8,16 @@ return {
     --   autoformat = false,
     -- },
     setup = {
+      tailwindcss = function()
+        Snacks.util.lsp.on(function(_, client)
+          if client.name == "tailwindcss" then
+            client.server_capabilities.workspace = client.server_capabilities.workspace or {}
+            client.server_capabilities.workspace.didChangeWatchedFiles = nil
+            client.capabilities.workspace = client.capabilities.workspace or {}
+            client.capabilities.workspace.didChangeWatchedFiles = nil
+          end
+        end)
+      end,
       eslint = function()
         Snacks.util.lsp.on(function(_, client)
           -- Disables autoformat for eslint and vtsls (leave it to conform to do it, which would probably use prettier)
