20354c75d08374fad6434eb98c8fa4aeb10d8363
Gabriel Duarte <gabrielqduarte@gmail.com>
2026-07-26 17:12:11 +0200

    work: remove unused files

--- work/linkFiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo ln -s -f ~/work/work.desktop ~/.local/share/applications/work.desktop
-sudo ln -s -f ~/work/FootWork.desktop ~/.local/share/applications/FootWork.desktop
-sudo ln -s -f ~/work/start-work-apps.sh ~/.local/bin/start-work-apps.sh
